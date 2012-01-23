2831a487ca969cac03643211ecdb6b2656cf0aef
Jeff LaBarge <jefflabarge@gmail.com>
2012-01-23 14:52:44 -0800

    go back to just testing 1 file

--- spec/merger/rails_spec.rb
+++ spec/merger/rails_spec.rb
@@ -21,46 +21,35 @@ describe "Vocab::Merger::Rails" do
 
     before( :each ) do
       # Simulate config/locals files (where files are merged)
-      @locales_dir = "#{vocab_root}/spec/tmp/merge"
-      FileUtils.rm_rf( @locales_dir ) if File.exists?( @locales_dir )
-      FileUtils.mkdir_p( @locales_dir )
+      @merge_dir = "#{vocab_root}/spec/tmp/merge"
+      FileUtils.rm_rf( @merge_dir ) if File.exists?( @merge_dir )
+      FileUtils.mkdir_p( @merge_dir )
+      FileUtils.cp_r( "#{vocab_root}/spec/data/locales/.", @merge_dir )
 
-      FileUtils.cp_r( "#{vocab_root}/spec/data/locales/.", @locales_dir )
+      @file = "#{@merge_dir}/en.yml"
+      @updates_dir = "#{vocab_root}/spec/data/translations"
+      @update_file = "#{@updates_dir}/en.yml"
+
+      merger = Vocab::Merger::Rails.new( @merge_dir, @updates_dir )
+      merger.merge_file( 'en.yml' )
+      @merged = YAML.load_file( @file )
+    end
+
+    it "merges updated translations" do
+      @merged[:marketing][:banner].should eql('this marketing message has changed')
+    end
+
+    it "merges new translations" do
+      @merged[:edu][:name].should eql('this is a new top level name')
+      @merged[:models][:product][:id_56][:description].should eql('This is a new nested description')
+      @merged[:models][:product][:id_56][:name].should eql('This is a new nested name')
+    end
+
+    it "retains unchanged translations" do
+      @merged[:menu][:first].should eql('First menu item')
     end
 
     it "skips file if matching old and update file not found"
-
-    describe 'top-level file' do
-
-      before( :each ) do
-        @file = "#{@locales_dir}/en.yml"
-        @updates_dir = "#{vocab_root}/spec/data/translations"
-        @update_file = "#{@updates_dir}/en.yml"
-
-        merger = Vocab::Merger::Rails.new( @locales_dir, @updates_dir )
-        merger.merge_file( 'en.yml' )
-        @merged = YAML.load_file( @file )
-      end
-
-       it "merges updated translations" do
-         @merged[ :marketing ][ :banner ].should eql( 'this marketing message has changed' )
-       end
-
-       it "merges new translations" do
-         @merged[ :edu ][ :name ].should eql( 'this is a new top level name' )
-         @merged[ :models ][ :product ][ :id_56 ][ :description ].should eql( 'This is a new nested description' )
-         @merged[ :models ][ :product ][ :id_56 ][ :name ].should eql( 'This is a new nested name' )
-       end
-
-       it "retains unchanged translations" do
-         @merged[ :menu ][ :first ].should eql( 'First menu item' )
-       end
-
-    end
-
-    describe 'nested file' do
-
-    end
 
   end
 
