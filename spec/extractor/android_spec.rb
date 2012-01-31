15a6dc20b367e1c9be5150f467bf7998d34c9939
Jeff LaBarge <jefflabarge@gmail.com>
2012-01-30 19:22:45 -0800

    fix spec

--- spec/extractor/android_spec.rb
+++ spec/extractor/android_spec.rb
@@ -10,9 +10,11 @@ describe "Vocab::Extractor::Android" do
 
     it "extracts hash of current string translations" do
       actual = Vocab::Extractor::Android.extract_current( @locale )
-      actual.should eql( { 'app_name'    => 'Kid Mode',
-                           'pd_app_name' => 'Parent Dashboard',
-                           'app_current' => 'current' } )
+      actual.should eql( { "app_name"   =>"Kid Mode",
+                           "delete"     =>"Delete",
+                           "cancel"     =>"Cancel",
+                           "app_current"=>"current",
+                           "pd_app_name"=>"Parent Dashboard" } )
     end
 
   end
