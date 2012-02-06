8294a5b2b02fa60c0954a90edfb21b35dd19ca74
Jeff LaBarge <jefflabarge@gmail.com>
2012-02-06 10:54:47 -0800

    validate single file for android

--- lib/vocab/validator/android.rb
+++ lib/vocab/validator/android.rb
@@ -2,6 +2,23 @@ module Vocab
   module Validator
     class Android
 
+      def initialize( locales_dir = nil )
+        @locales_dir = locales_dir || 'res'
+      end
+
+      def validate_file( path )
+        other = Vocab::Translator::Android.hash_from_xml( path ).keys
+
+        result = {}
+        result[ :missing ] = ( english_keys - other ).sort
+        result[ :extra ] = ( other - english_keys ).sort
+        return result
+      end
+
+      def english_keys
+        return Vocab::Translator::Android.english_keys( @locales_dir )
+      end
+
     end
   end
 end
--- spec/validator/android_spec.rb
+++ spec/validator/android_spec.rb
@@ -2,7 +2,27 @@ require "spec_helper"
 
 describe "Vocab::Validator::Android" do
 
-  describe '' do
+  describe 'validate_file' do
+
+    before( :each ) do
+      @locales_dir = "#{vocab_root}/spec/data/android/locales"
+      @validator = Vocab::Validator::Android.new( @locales_dir )
+      @path = "#{vocab_root}/spec/data/android/locales/values-es/strings.xml"
+    end
+
+    it 'returns a hash containing the missing keys' do
+      result = @validator.validate_file( @path )
+      result[ :missing ].should eql( [ "cancel", "delete", "not_in_es" ] )
+    end
+
+    it 'returns a hash containing the extra keys' do
+      spanish = { 'foo' => 'bar', 'extra' => 'coffee', 'stuff' => 'clutter' }
+      Vocab::Translator::Android.should_receive( :hash_from_xml ).and_return( spanish )
+      @validator.should_receive( :english_keys ).at_least(:once).and_return( [ 'foo' ] )
+
+      result = @validator.validate_file( @path )
+      result[ :extra ].should eql( [ 'extra', 'stuff' ] )
+    end
 
   end
 
