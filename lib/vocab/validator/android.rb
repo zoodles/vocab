ed3bf02efb6aceade7d9b36254a38412f2b7d5f9
Jeff LaBarge <jefflabarge@gmail.com>
2012-02-06 11:44:12 -0800

    more pieces of the android validation tool

--- lib/vocab/validator/android.rb
+++ lib/vocab/validator/android.rb
@@ -6,6 +6,27 @@ module Vocab
         @locales_dir = locales_dir || 'res'
       end
 
+      def validate
+        files_to_validate.each do |path|
+          validation = validate_file( path )
+          print( path, validation )
+        end
+      end
+
+      def print( path, validation )
+        Vocab.ui.say( "Validating: #{path}" )
+
+        unless validation[ :missing ].empty?
+          Vocab.ui.say( "  Missing keys:" )
+          validation[ :missing ].each { |missing| Vocab.ui.say( "    - #{missing}" ) }
+        end
+
+        unless validation[ :extra ].empty?
+          Vocab.ui.say( "  Extra keys:" )
+          validation[ :extra ].each { |extra| Vocab.ui.say( "    + #{extra}" ) }
+        end
+      end
+
       def validate_file( path )
         other = Vocab::Translator::Android.hash_from_xml( path ).keys
 
@@ -17,6 +38,10 @@ module Vocab
 
       def english_keys
         return Vocab::Translator::Android.english_keys( @locales_dir )
+      end
+
+      def files_to_validate
+        return Dir.glob( "#{@locales_dir}/values-*/strings.xml" )
       end
 
     end
--- spec/validator/android_spec.rb
+++ spec/validator/android_spec.rb
@@ -2,11 +2,14 @@ require "spec_helper"
 
 describe "Vocab::Validator::Android" do
 
+  before( :each ) do
+    @locales_dir = "#{vocab_root}/spec/data/android/locales"
+    @validator = Vocab::Validator::Android.new( @locales_dir )
+  end
+
   describe 'validate_file' do
 
     before( :each ) do
-      @locales_dir = "#{vocab_root}/spec/data/android/locales"
-      @validator = Vocab::Validator::Android.new( @locales_dir )
       @path = "#{vocab_root}/spec/data/android/locales/values-es/strings.xml"
     end
 
@@ -22,6 +25,30 @@ describe "Vocab::Validator::Android" do
 
       result = @validator.validate_file( @path )
       result[ :extra ].should eql( [ 'extra', 'stuff' ] )
+    end
+
+  end
+
+  describe 'files_to_validate' do
+
+    it 'returns the locale files to validate' do
+      files = [ "#{@locales_dir}/values-es/strings.xml" ]
+      @validator.files_to_validate.should eql( files )
+    end
+
+  end
+
+  describe 'validate' do
+
+    it 'validates android locales files' do
+      files = @validator.files_to_validate
+      files.each { |file| @validator.should_receive( :validate_file ).with( file ) }
+      @validator.should_receive( :print ).exactly( files.size ).times
+      @validator.validate
+    end
+
+    it 'prints without exception' do
+      @validator.validate
     end
 
   end
